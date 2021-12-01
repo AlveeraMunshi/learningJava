142013c64d7c422331b736d03d33105c163cb6d7
Alveera Munshi <sporty.alveera@gmail.com>
2021-12-01 08:40:11 -0500

    organization and spinner lesson

---
+++ CSMAD/Spinners/spinnerLesson/app/src/main/java/com/example/spinnerlesson/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.spinnerlesson;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    Spinner spinner;
+    TextView selection;
+    ArrayList<String> list;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        spinner = findViewById(R.id.spinner);
+        selection = findViewById(R.id.textView);
+
+        list = new Array<String>();
+        list.add("Jim");
+        list.add("John");
+        list.add("Jen");
+        list.add("Jill");
+        list.add("Jeff");
+        list.add("Josh");
+        list.add("Jimmy");
+        list.add("Jack");
+
+        ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item);
+        spinner.setAdapter(spinnerAdapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                selection.setText(list.get(i));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+    }
+}

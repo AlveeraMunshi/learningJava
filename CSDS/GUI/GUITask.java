bdee5c6dadd8c612c7d1f194557b0469cee931f5
Alveera Munshi <sporty.alveera@gmail.com>
2023-02-23 09:05:04 -0500

    guiiiiii

--- CSDS/GUI/GUITask.java
+++ CSDS/GUI/GUITask.java
@@ -6,10 +6,11 @@ import java.awt.*;
 import java.awt.event.*;
 import java.util.ArrayList;
 
-public class GUITask extends JFrame{
+public class GUITask implements ActionListener{
+    JFrame frame;
     JPanel kingPanel, babyPanel;
     GridLayout bLayout, mLayout, kLayout;
-    JToggleButton north, south, east, west, reset;
+    JButton north, south, east, west, reset;
     JMenuBar menuBar;
     JMenu font, size, color, bgcolor, olcolor;
     JTextArea textArea;
@@ -29,26 +30,44 @@ public class GUITask extends JFrame{
     int currentSize = fontSizesInt[0];
     //4 Color arrays: border color, text color, outline color, text background color
     Color[] colors = {Color.RED, Color.BLUE, Color.BLACK, Color.GREEN, Color.YELLOW, Color.ORANGE, Color.PINK, Color.CYAN, Color.MAGENTA, Color.WHITE, Color.GRAY, Color.LIGHT_GRAY, Color.DARK_GRAY, Color.BLACK};
-    //1 font array
-    Font[] fonts = {new Font("Arial", Font.PLAIN, 18), new Font("Times New Roman", Font.PLAIN, 18), new Font("Courier New", Font.PLAIN, 18)};
-    Font currentFont = fonts[0];
+    ArrayList<Font> fontList = new ArrayList<Font>();
+    Font currentFont = new Font("Arial", Font.PLAIN, 18);
     public GUITask()
     {
-        this.setSize(1000, 1000); //set size of window
-        this.setLayout(new BorderLayout()); //set layout of window
+        try
+        {
+            UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
+            //UIManager.setLookAndFeel("com.sun.java.swing.plaf.motif.MotifLookAndFeel");
+
+        }
+        catch (Exception e)
+        {
+        }
+        //vars
+        fontList.add(new Font("Arial", Font.PLAIN, 18));
+        fontList.add(new Font("Times New Roman", Font.ROMAN_BASELINE, 18));
+        fontList.add(new Font("Courier New", Font.PLAIN, 18));
+        //window
+        frame = new JFrame(); //create window
+        frame.setSize(1500, 1000); //set size of window
+        frame.setLayout(new BorderLayout()); //set layout of window
         kingPanel = new JPanel(); //big panel for entire window
-        kLayout = new GridLayout(2, 1); //2 rows, 1 column
-        kingPanel.setLayout(kLayout);
         babyPanel = new JPanel(); //small panel for buttons/menu
         //buttons
-        north = new JToggleButton("North");
-        south = new JToggleButton("South");
-        east = new JToggleButton("East");
-        west = new JToggleButton("West");
-        reset = new JToggleButton("Reset");
+        north = new JButton("North");
+        north.addActionListener(this);
+        south = new JButton("South");
+        south.addActionListener(this);
+        east = new JButton("East");
+        east.addActionListener(this);
+        west = new JButton("West");
+        west.addActionListener(this);
+        reset = new JButton("Reset");
+        reset.addActionListener(this);
         menuBar = new JMenuBar();
         //menus
         font = new JMenu("Font");
+        font.setOpaque(false);
         size = new JMenu("Size");
         color = new JMenu("Color");
         bgcolor = new JMenu("Background Color");
@@ -70,104 +89,58 @@ public class GUITask extends JFrame{
         buttonOutlineColors.add(new JMenuItem("Red"));
         buttonOutlineColors.add(new JMenuItem("Blue"));
         buttonOutlineColors.add(new JMenuItem("Random Color"));
-        //action listeners
-        fontOptions.get(0).addActionListener(new ActionListener()
+        //iter through menuitems and add to menu
+        for (JMenuItem o : fontOptions)
         {
-            public void actionPerformed(ActionEvent e)
-            {
-                currentFont = fonts[0];
-                textArea.setFont(currentFont);
-            }
-        });
-        north.addActionListener(new ActionListener()
+            JMenuItem temp = o;
+            temp.setFont(new Font(temp.getText(), Font.PLAIN, 18));
+            temp.addActionListener(this);
+            font.add(temp);
+        }
+        for (JMenuItem o : fontSizes)
         {
-            public void actionPerformed(ActionEvent e)
-            {
-                if(north.isSelected())
-                {
-                    //textArea.append("North");
-                    System.out.print("North");
-                    kingPanel.remove(babyPanel);
-                    bLayout = new GridLayout(1,4);
-                    mLayout = new GridLayout(1,6);
-                    babyPanel.setLayout(bLayout);
-                    kingPanel.add(babyPanel, BorderLayout.NORTH);
-                }
-            }
-        });
-        south.addActionListener(new ActionListener()
+            o.addActionListener(this);
+            size.add(o);
+        }
+        for (JMenuItem o : textColors)
         {
-            public void actionPerformed(ActionEvent e)
-            {
-                if(south.isSelected())
-                {
-                    //textArea.append("South");
-                    System.out.print("South");
-                    kingPanel.remove(babyPanel);
-                    bLayout = new GridLayout(1,4);
-                    mLayout = new GridLayout(1,6);
-                    babyPanel.setLayout(bLayout);
-                    kingPanel.add(babyPanel, BorderLayout.SOUTH);
-                }
-            }
-        });
-        east.addActionListener(new ActionListener()
+            o.addActionListener(this);
+            color.add(o);
+        }
+        for (JMenuItem o : textBGColors)
         {
-            public void actionPerformed(ActionEvent e)
-            {
-                if(east.isSelected())
-                {
-                    //textArea.append("East");
-                    System.out.println("East");
-                    kingPanel.remove(babyPanel);
-                    bLayout = new GridLayout(4,1);
-                    mLayout = new GridLayout(6,1);
-                    babyPanel.setLayout(bLayout);
-                    kingPanel.add(babyPanel, BorderLayout.EAST);
-                }
-            }
-        });
-        west.addActionListener(new ActionListener()
+            o.addActionListener(this);
+            bgcolor.add(o);
+        }
+        for (JMenuItem o : buttonOutlineColors)
         {
-            public void actionPerformed(ActionEvent e)
-            {
-                if(west.isSelected())
-                {
-                    //textArea.append("West");
-                    System.out.println("West");
-                    kingPanel.remove(babyPanel);
-                    bLayout = new GridLayout(4,1);
-                    mLayout = new GridLayout(6,1);
-                    babyPanel.setLayout(bLayout);
-                    kingPanel.add(babyPanel, BorderLayout.WEST);
-                }
-            }
-        });
-        reset.addActionListener(new ActionListener()
-        {
-            public void actionPerformed(ActionEvent e)
-            {
-                if(reset.isSelected())
-                {
-                    //textArea.append("Reset");
-                    //kingPanel.remove(babyPanel);
-                }
-            }
-        });
+            o.addActionListener(this);
+            olcolor.add(o);
+        }
+        //jtextarea
+        textArea = new JTextArea();
+        textArea.setFont(new Font("Arial", Font.PLAIN, 18));
+        frame.add(textArea, BorderLayout.CENTER);
 
 
         //setup
-        babyPanel.setLayout(new GridLayout(1,10));
+        babyPanel.setLayout(new GridLayout(1,4));
+        menuBar.setLayout(new GridLayout(1, 6));
         addParts();
-        kingPanel.add(babyPanel, BorderLayout.NORTH);
-        this.add(kingPanel);
+        kingPanel.setLayout(new GridLayout(1,2));
+        kingPanel.add(babyPanel);
+        kingPanel.add(menuBar);
+        frame.add(kingPanel, BorderLayout.NORTH);
 
-        this.setVisible(true);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.revalidate(); //repaint for GUI
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //frame.revalidate(); //repaint for GUI
     }
+
+
     public void addParts()
     {
+
         babyPanel.add(north);
         babyPanel.add(south);
         babyPanel.add(east);
@@ -178,10 +151,279 @@ public class GUITask extends JFrame{
         menuBar.add(bgcolor);
         menuBar.add(olcolor);
         menuBar.add(reset);
-        babyPanel.add(menuBar);
+       
+
+    }
+    public void updateFont(Font currentFont)
+    {
+        Font buttonFont = new Font(currentFont.getName(), Font.PLAIN, 18);
+        textArea.setFont(currentFont);
+        north.setFont(buttonFont);
+        south.setFont(buttonFont);
+        east.setFont(buttonFont);
+        west.setFont(buttonFont);
+    }
+    public void updateTextColor(Color currentColor)
+    {
+        textArea.setForeground(currentColor);
+        north.setForeground(currentColor);
+        south.setForeground(currentColor);
+        east.setForeground(currentColor);
+        west.setForeground(currentColor);
+        font.setForeground(currentColor);
+        size.setForeground(currentColor);
+        color.setForeground(currentColor);
+        bgcolor.setForeground(currentColor);
+        olcolor.setForeground(currentColor);
+        reset.setForeground(currentColor);
     }
     public static void main (String[] args)
     {
         GUITask gui = new GUITask();
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        if (e.getSource() == east)
+        {
+            System.out.println("East");
+            frame.remove(kingPanel);
+            kingPanel.remove(babyPanel);
+            kingPanel.remove(menuBar);
+            kLayout = new GridLayout(2,1);
+            bLayout = new GridLayout(4,1);
+            mLayout = new GridLayout(6,1);
+            babyPanel.setLayout(bLayout);
+            kingPanel.setLayout(kLayout);
+            menuBar.setLayout(mLayout);
+            kingPanel.add(babyPanel, BorderLayout.EAST);
+            kingPanel.add(menuBar, BorderLayout.EAST);
+            frame.add(kingPanel, BorderLayout.EAST);
+        }
+        else if (e.getSource() == west)
+        {
+            System.out.println("West");
+            frame.remove(kingPanel);
+            kingPanel.remove(babyPanel);
+            kingPanel.remove(menuBar);
+            kLayout = new GridLayout(2,1);
+            bLayout = new GridLayout(4,1);
+            mLayout = new GridLayout(6,1);
+            babyPanel.setLayout(bLayout);
+            kingPanel.setLayout(kLayout);
+            menuBar.setLayout(mLayout);
+            kingPanel.add(babyPanel, BorderLayout.WEST);
+            kingPanel.add(menuBar, BorderLayout.WEST);
+            frame.add(kingPanel, BorderLayout.WEST);
+        }
+        else if (e.getSource() == north)
+        {
+            System.out.print("North");
+            frame.remove(kingPanel);
+            kingPanel.remove(babyPanel);
+            kingPanel.remove(menuBar);
+            kLayout = new GridLayout(1, 2);
+            bLayout = new GridLayout(1,4);
+            mLayout = new GridLayout(1,6);
+            babyPanel.setLayout(bLayout);
+            kingPanel.setLayout(kLayout);
+            menuBar.setLayout(mLayout);
+            kingPanel.add(babyPanel, BorderLayout.NORTH);
+            kingPanel.add(menuBar, BorderLayout.NORTH);
+            frame.add(kingPanel, BorderLayout.NORTH);
+        }
+        else if (e.getSource() == south)
+        {
+            System.out.print("South");
+            frame.remove(kingPanel);
+            kingPanel.remove(babyPanel);
+            kingPanel.remove(menuBar);
+            kLayout = new GridLayout(1, 2);
+            bLayout = new GridLayout(1,4);
+            mLayout = new GridLayout(1,6);
+            babyPanel.setLayout(bLayout);
+            kingPanel.setLayout(kLayout);
+            menuBar.setLayout(mLayout);
+            kingPanel.add(babyPanel, BorderLayout.SOUTH);
+            kingPanel.add(menuBar, BorderLayout.SOUTH);
+            frame.add(kingPanel, BorderLayout.SOUTH);
+        }
+        else if (e.getSource() == reset)
+        {
+            currentFont = new Font("Arial", Font.PLAIN, 18);
+            updateFont(currentFont);
+            north.setBorderPainted(false);
+            south.setBorderPainted(false);
+            east.setBorderPainted(false);
+            west.setBorderPainted(false);
+            reset.setBorderPainted(false);
+            updateTextColor(Color.BLACK);
+            textArea.setBackground(Color.WHITE);
+            frame.remove(kingPanel);
+            kingPanel.remove(babyPanel);
+            kingPanel.remove(menuBar);
+            bLayout = new GridLayout(1,4);
+            kLayout = new GridLayout(1, 2);
+            mLayout = new GridLayout(1,6);
+            babyPanel.setLayout(bLayout);
+            kingPanel.setLayout(kLayout);
+            kingPanel.add(babyPanel);
+            kingPanel.add(menuBar);
+            frame.add(kingPanel, BorderLayout.NORTH);
+        }
+        else if (e.getSource() == fontOptions.get(0))
+        {
+            currentFont = new Font(fontNames[0], currentFont.getStyle(), currentSize);
+            updateFont(currentFont);
+        }
+        else if (e.getSource() == fontOptions.get(1))
+        {
+            currentFont = new Font(fontNames[1], currentFont.getStyle(), currentSize);
+            updateFont(currentFont);
+        }
+        else if (e.getSource() == fontOptions.get(2))
+        {
+            currentFont = new Font(fontNames[2], currentFont.getStyle(), currentSize);
+            updateFont(currentFont);
+        }
+        else if (e.getSource() == fontSizes.get(0))
+        {
+            currentSize = fontSizesInt[0];
+            currentFont = new Font(currentFont.getName(), currentFont.getStyle(), currentSize);
+            updateFont(currentFont);
+        }
+        else if (e.getSource() == fontSizes.get(1))
+        {
+            currentSize = fontSizesInt[1];
+            currentFont = new Font(currentFont.getName(), currentFont.getStyle(), currentSize);
+            updateFont(currentFont);
+        }
+        else if (e.getSource() == fontSizes.get(2))
+        {
+            currentSize = fontSizesInt[2];
+            currentFont = new Font(currentFont.getName(), currentFont.getStyle(), currentSize);
+            updateFont(currentFont);
+        }
+        else if (e.getSource() == textColors.get(0))
+        {
+            updateTextColor(colors[0]);
+        }
+        else if (e.getSource() == textColors.get(1))
+        {
+            updateTextColor(colors[1]);
+        }
+        else if (e.getSource() == textColors.get(2))
+        {
+            Color rc = colors[(int)(Math.random()*colors.length)];
+            updateTextColor(rc);
+        }
+        else if (e.getSource() == textBGColors.get(0))
+        {
+            textArea.setBackground(colors[0]);
+        }
+        else if (e.getSource() == textBGColors.get(1))
+        {
+            textArea.setBackground(colors[1]);
+        }
+        else if (e.getSource() == textBGColors.get(2))
+        {
+            Color rc = colors[(int)(Math.random()*colors.length)];
+            textArea.setBackground(rc);
+        }
+        else if (e.getSource() == north)
+        {
+            System.out.println("North");
+            north.setBorderPainted(true);
+            south.setBorderPainted(false);
+            east.setBorderPainted(false);
+            west.setBorderPainted(false);
+            reset.setBorderPainted(false);
+        }
+        else if (e.getSource() == south)
+        {
+            System.out.println("South");
+            north.setBorderPainted(false);
+            south.setBorderPainted(true);
+            east.setBorderPainted(false);
+            west.setBorderPainted(false);
+            reset.setBorderPainted(false);
+        }
+        else if (e.getSource() == east)
+        {
+            System.out.println("East");
+            north.setBorderPainted(false);
+            south.setBorderPainted(false);
+            east.setBorderPainted(true);
+            west.setBorderPainted(false);
+            reset.setBorderPainted(false);
+        }
+        else if (e.getSource() == west)
+        {
+            System.out.println("West");
+            north.setBorderPainted(false);
+            south.setBorderPainted(false);
+            east.setBorderPainted(false);
+            west.setBorderPainted(true);
+            reset.setBorderPainted(false);
+        }
+        else if (e.getSource() == reset)
+        {
+            System.out.println("Reset");
+            north.setBorderPainted(false);
+            south.setBorderPainted(false);
+            east.setBorderPainted(false);
+            west.setBorderPainted(false);
+            reset.setBorderPainted(true);
+        }
+        else if (e.getSource() == buttonOutlineColors.get(0))
+        {
+            north.setBorderPainted(false);
+            south.setBorderPainted(false);
+            east.setBorderPainted(false);
+            west.setBorderPainted(false);
+            reset.setBorderPainted(false);
+        }
+        else if (e.getSource() == buttonOutlineColors.get(1))
+        {
+            north.setBorderPainted(true);
+            south.setBorderPainted(true);
+            east.setBorderPainted(true);
+            west.setBorderPainted(true);
+            reset.setBorderPainted(true);
+            north.setBorder(new LineBorder(colors[0]));
+            south.setBorder(new LineBorder(colors[0]));
+            east.setBorder(new LineBorder(colors[0]));
+            west.setBorder(new LineBorder(colors[0]));
+            reset.setBorder(new LineBorder(colors[0]));
+        }
+        else if (e.getSource() == buttonOutlineColors.get(2))
+        {
+            north.setBorderPainted(true);
+            south.setBorderPainted(true);
+            east.setBorderPainted(true);
+            west.setBorderPainted(true);
+            reset.setBorderPainted(true);
+            north.setBorder(new LineBorder(colors[1]));
+            south.setBorder(new LineBorder(colors[1]));
+            east.setBorder(new LineBorder(colors[1]));
+            west.setBorder(new LineBorder(colors[1]));
+            reset.setBorder(new LineBorder(colors[1]));
+        }
+        else if (e.getSource() == buttonOutlineColors.get(3))
+        {
+            north.setBorderPainted(true);
+            south.setBorderPainted(true);
+            east.setBorderPainted(true);
+            west.setBorderPainted(true);
+            reset.setBorderPainted(true);
+            north.setBorder(new LineBorder(colors[(int)(Math.random()*colors.length)]));
+            south.setBorder(new LineBorder(colors[(int)(Math.random()*colors.length)]));
+            east.setBorder(new LineBorder(colors[(int)(Math.random()*colors.length)]));
+            west.setBorder(new LineBorder(colors[(int)(Math.random()*colors.length)]));
+            reset.setBorder(new LineBorder(colors[(int)(Math.random()*colors.length)]));
+        }
+        frame.revalidate();
     }
 }
